42173976f16e96e1aaff87be23ad6d76186c9c41
AndPrim <andreyminoss@gmail.com>
2024-01-11 16:16:44 +0500

    ClassTwoCommit

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -1,7 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -20,18 +19,33 @@ public class DeliveryCardTest {
     @Test
     void openDeliveryCardTest() {
         open("http://localhost:9999/");
-//        $(byText("Карта с доставкой!"));
-        $("[data-test-id=city] input").setValue("Че");
-        SelenideElement el = $$(".popup__container").last();
-        el.$$(".menu-item__control").find(exactText("Челябинск")).click();
-        String dateNew = dateGenerate(7, "dd.MM.yyyy");
+        $("[data-test-id=city] input").setValue("Челябинск");
+        String date = dateGenerate(5, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME),
                 Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(dateNew);
-//        String xpathOne = "//*[date()=" + dateNew + "  ]";
-//        SelenideElement date = $("[data-test-id='date']");
-//        date.$(".icon_name_calendar").click();
-//        date.$(By.xpath(xpathOne)).click();
+        $("[data-test-id='date'] input").setValue(date);
+        $("[data-test-id='name'] input").setValue("Ступин Иван");
+        $("[data-test-id='phone'] input").setValue("+79998887766");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $(".notification__title")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.text("Успешно!"));
+        $(".notification__content")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date));
+    }
+    @Test
+    void openDeliveryCardTestCalendar() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Че");
+        $$(".menu-item__control").find(exactText("Челябинск")).click();
+        String dateNew = dateGenerate(7, "dd.MM.yyyy");
+        $("[data-test-id='date'] button").click();
+        if(!dateGenerate(0, "MM").equals(dateGenerate(7, "MM"))){
+        $("[data-step='1']").click();
+        }
+        $$(".calendar__day").find(exactText(dateGenerate(7,"dd"))).click();
         $("[data-test-id='name'] input").setValue("Ступин Иван");
         $("[data-test-id='phone'] input").setValue("+79998887766");
         $("[data-test-id='agreement']").click();
